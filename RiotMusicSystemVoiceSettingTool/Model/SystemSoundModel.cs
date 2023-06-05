5be65fed528f1c3fe854a5624f9fdb1c1d6a2884
rinjugatla <rin.jugatla@gmail.com>
2023-06-06 01:19:17 +0900

    Windows, Explorerのサウンドを切り分け、レジストリパスを取得する機能をを追加

--- RiotMusicSystemVoiceSettingTool/Model/SystemSoundModel.cs
+++ RiotMusicSystemVoiceSettingTool/Model/SystemSoundModel.cs
@@ -8,16 +8,8 @@ namespace RiotMusicSystemVoiceSettingTool.Model
 {
     internal class SystemSoundModel
     {
-        public enum SoundProgramType
+        public enum WindowsSoundType
         {
-            Windows,
-            Explorer,
-            Other
-        }
-
-        public enum SoundType
-        {
-            #region Windows
             /// <summary>NFP完了</summary>
             NfpFinish,
             /// <summary>NFP接続</summary>
@@ -50,10 +42,10 @@ namespace RiotMusicSystemVoiceSettingTool.Model
             ProgramError,
             /// <summary>プログラム終了</summary>
             ProgramExit,
-            /// <summary>プログラム開始</summary>
+            /// <summary>プログラム起動</summary>
             ProgramStart,
             /// <summary>メッセージ(問い合わせ)</summary>
-            MessageInquiry,
+            MessageQuestion,
             /// <summary>情報メッセージ</summary>
             MessageInfomation,
             /// <summary>警告メッセージ</summary>
@@ -88,9 +80,10 @@ namespace RiotMusicSystemVoiceSettingTool.Model
             Notification,
             /// <summary>選択</summary>
             Select,
-            #endregion
+        }
 
-            #region Explorer
+        public enum ExplorerSoundType
+        {
             /// <summary>ごみ箱を空にする</summary>
             EmptyTrash,
             /// <summary>ナビゲーション終了</summary>
@@ -105,7 +98,116 @@ namespace RiotMusicSystemVoiceSettingTool.Model
             MenuItemMove,
             /// <summary>通知バー</summary>
             NotificationBar,
-            #endregion
+        }
+
+        /// <summary>レジストリパスを取得</summary>
+        public static string RegistryPath(WindowsSoundType type)
+        {
+            string @base = @"AppEvents\Schemes\Apps\.Default\";
+            string key = "";
+            switch (type)
+            {
+                case WindowsSoundType.NfpFinish:
+                    key = "Notification.Proximity"; break;
+                case WindowsSoundType.NfpConnect:
+                    key = "ProximityConnection"; break;
+                case WindowsSoundType.WindowsThemeChange:
+                    key = "ChangeTheme"; break;
+                case WindowsSoundType.WindowsUserAccountControl:
+                    key = "WindowsUAC"; break;
+                case WindowsSoundType.InstanceMessageNotification:
+                    key = "Notification.IM"; break;
+                case WindowsSoundType.SystemError:
+                    key = "SystemHand"; break;
+                case WindowsSoundType.SystemNotification:
+                    key = "SystemNotification"; break;
+                case WindowsSoundType.ToolBarBandShow:
+                    key = "ShowBand"; break;
+                case WindowsSoundType.DesktopMailNotification:
+                    key = "MailBeep"; break;
+                case WindowsSoundType.DeviceDisconnect:
+                    key = "DeviceDisconnect"; break;
+                case WindowsSoundType.DeviceConnect:
+                    key = "DeviceConnect"; break;
+                case WindowsSoundType.DeviceConnectError:
+                    key = "DeviceFail"; break;
+                case WindowsSoundType.BatteryLow:
+                    key = "LowBatteryAlarm"; break;
+                case WindowsSoundType.BatteryEmpty:
+                    key = "CriticalBatteryAlarm"; break;
+                case WindowsSoundType.ProgramError:
+                    key = "AppGPFault"; break;
+                case WindowsSoundType.ProgramExit:
+                    key = "Close"; break;
+                case WindowsSoundType.ProgramStart:
+                    key = "Open"; break;
+                case WindowsSoundType.MessageQuestion:
+                    key = "MessageQuestion"; break;
+                case WindowsSoundType.MessageInfomation:
+                    key = "SystemAsterisk"; break;
+                case WindowsSoundType.MessageWarning:
+                    key = "SystemExclamation"; break;
+                case WindowsSoundType.MessageShake:
+                    key = "MessageNudge"; break;
+                case WindowsSoundType.MenuCommand:
+                    key = "MenuCommand"; break;
+                case WindowsSoundType.MenuPopup:
+                    key = "MenuPopup"; break;
+                case WindowsSoundType.GeneralWarning:
+                    key = ".Default"; break;
+                case WindowsSoundType.ScheduleAlarm:
+                    key = "Notification.Reminder"; break;
+                case WindowsSoundType.ExpansionUndo:
+                    key = "RestoreUp"; break;
+                case WindowsSoundType.ShrinkUndo:
+                    key = "RestoreDown"; break;
+                case WindowsSoundType.PrintFinish:
+                    key = "PrintComplete"; break;
+                case WindowsSoundType.NewMessageNotification:
+                    key = "Notification.SMS"; break;
+                case WindowsSoundType.NewFaxNotification:
+                    key = "FaxBeep"; break;
+                case WindowsSoundType.NewMailNotification:
+                    key = "Mail"; break;
+                case WindowsSoundType.MaxWIndowSize:
+                    key = "Maximize"; break;
+                case WindowsSoundType.MinWindowSize:
+                    key = "Minimize"; break;
+                case WindowsSoundType.Notification:
+                    key = "Notification.Default"; break;
+                case WindowsSoundType.Select:
+                    key = "CCSelect"; break;
+            }
+
+            string path = @base + key;
+            return path;
+        }
+
+        /// <summary>レジストリパスを取得</summary>
+        public string RegistryPath(ExplorerSoundType type)
+        {
+            string @base = @"AppEvents\Schemes\Apps\Explorer\";
+            string key = "";
+            switch (type)
+            {
+                case ExplorerSoundType.EmptyTrash:
+                    key = "EmptyRecycleBin"; break;
+                case ExplorerSoundType.NavigationFinish:
+                    key = "ActivatingDocument"; break;
+                case ExplorerSoundType.NavigationStart:
+                    key = "Navigating"; break;
+                case ExplorerSoundType.FeedDiscover:
+                    key = "FeedDiscovered"; break;
+                case ExplorerSoundType.PopupWindowBlock:
+                    key = "BlockedPopup"; break;
+                case ExplorerSoundType.MenuItemMove:
+                    key = "MoveMenuItem"; break;
+                case ExplorerSoundType.NotificationBar:
+                    key = "SecurityBand"; break;
+            }
+
+            string path = @base + key;
+            return path;
         }
     }
 }
--- RiotMusicSystemVoiceSettingTool/Model/SystemVoiceModel.cs
+++ RiotMusicSystemVoiceSettingTool/Model/SystemVoiceModel.cs
@@ -101,48 +101,60 @@ namespace RiotMusicSystemVoiceSettingTool.Model
         /// <summary>ボイスファイル名を取得</summary>
         /// <param name="sound">ボイス種類</param>
         /// <exception cref="ArgumentException">未対応のボイス</exception>
-        public string VoiceFileName(SoundType sound)
+        public string VoiceFileName(WindowsSoundType sound)
         {
             switch (sound)
             {
-                case SoundType.SystemError:
+                case WindowsSoundType.SystemError:
                     return $"{ShortName}-PC-01-システムエラー.wav";
-                case SoundType.SystemNotification:
+                case WindowsSoundType.SystemNotification:
                     return $"{ShortName}-PC-02-システム通知.wav";
-                case SoundType.DeviceConnect:
+                case WindowsSoundType.DeviceConnect:
                     return $"{ShortName}-PC-03-デバイスの接続.wav";
-                case SoundType.DeviceDisconnect:
+                case WindowsSoundType.DeviceDisconnect:
                     return $"{ShortName}-PC-04-デバイスの切断.wav";
-                case SoundType.DeviceConnectError:
+                case WindowsSoundType.DeviceConnectError:
                     return $"{ShortName}-PC-05-デバイスの接続の失敗.wav";
-                case SoundType.BatteryLow:
+                case WindowsSoundType.BatteryLow:
                     return $"{ShortName}-PC-06-バッテリ低下アラーム.wav";
-                case SoundType.BatteryEmpty:
+                case WindowsSoundType.BatteryEmpty:
                     return $"{ShortName}-PC-07-バッテリ切れアラーム.wav";
-                case SoundType.ProgramError:
+                case WindowsSoundType.ProgramError:
                     return $"{ShortName}-PC-08-プログラムエラー.wav";
-                case SoundType.MessageInfomation:
+                case WindowsSoundType.MessageInfomation:
                     return $"{ShortName}-PC-09-メッセージ（情報）.wav";
-                case SoundType.MessageWarning:
+                case WindowsSoundType.MessageWarning:
                     return $"{ShortName}-PC-10-メッセージ（警告）.wav";
-                case SoundType.GeneralWarning:
+                case WindowsSoundType.GeneralWarning:
                     return $"{ShortName}-PC-11-一般の警告音.wav";
-                case SoundType.PrintFinish:
+                case WindowsSoundType.PrintFinish:
                     return $"{ShortName}-PC-12-印刷完了.wav";
-                case SoundType.NewMailNotification:
+                case WindowsSoundType.NewMailNotification:
                     return $"{ShortName}-PC-13-新着メールの通知.wav";
-                case SoundType.MaxWIndowSize:
+                case WindowsSoundType.MaxWIndowSize:
                     return $"{ShortName}-PC-14-ウィンドウの最大化.wav";
-                case SoundType.MinWindowSize:
+                case WindowsSoundType.MinWindowSize:
                     return $"{ShortName}-PC-15-ウィンドウの最小化.wav";
-                case SoundType.Notification:
+                case WindowsSoundType.Notification:
                     return $"{ShortName}-PC-16-通知.wav";
-                case SoundType.EmptyTrash:
-                    return $"{ShortName}-PC-17-ごみ箱を空にする.wav";
-                case SoundType.PopupWindowBlock:
-                    return $"{ShortName}-PC-18-ポップアップウィンドウのブロック.wav";
-                case SoundType.WindowsThemeChange:
+                case WindowsSoundType.WindowsThemeChange:
                     return $"{ShortName}-PC-19-Window テーマの変更.wav";
+                default:
+                    throw new ArgumentException("未対応のボイス種類");
+            }
+        }
+
+        /// <summary>ボイスファイル名を取得</summary>
+        /// <param name="sound">ボイス種類</param>
+        /// <exception cref="ArgumentException">未対応のボイス</exception>
+        public string VoiceFileName(ExplorerSoundType sound)
+        {
+            switch (sound)
+            {
+                case ExplorerSoundType.EmptyTrash:
+                    return $"{ShortName}-PC-17-ごみ箱を空にする.wav";
+                case ExplorerSoundType.PopupWindowBlock:
+                    return $"{ShortName}-PC-18-ポップアップウィンドウのブロック.wav";
                 default:
                     throw new ArgumentException("未対応のボイス種類");
             }
